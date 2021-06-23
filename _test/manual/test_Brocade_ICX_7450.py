4e5f654572d504c8bb42b980584e16cb0defe3de
Shawn Duong <shawnduong@protonmail.com>
2021-06-22 20:50:43 -0700

    Created manual unit tests for Brocade ICX 7450 ping functionality.

--- _test/manual/test_Brocade_ICX_7450.py
+++ _test/manual/test_Brocade_ICX_7450.py
@@ -7,6 +7,17 @@ sys.path.insert(0, "../../")
 from _common import config
 from Brocade.Brocade_ICX_7450 import Brocade_ICX_7450
 
+def init(ip, user):
+	"""
+	Shared switch initialization function returning the device object.
+	"""
+
+	conf      = config.config()
+	password  = getpass.getpass("(Test) Password: ")
+	secret    = getpass.getpass("(Test) Secret: ")
+
+	return Brocade_ICX_7450("ic", ip, conf, user, password, secret)
+
 def test_constructor(args):
 
 	conf = config.config()
@@ -30,18 +41,24 @@ def test_constructor(args):
 
 def test_send_cmd(args):
 
-	conf = config.config()
-
 	# Initialize and connect.
-	password  = getpass.getpass("(Test) Password: ")
-	secret    = getpass.getpass("(Test) Secret: ")
-	device    = Brocade_ICX_7450("ic", args[0], conf, args[1], password, secret)
+	device = init(args[0], args[1])
 
 	# Send command.
 	if len(args) == 3:
 		device.send_cmd(args[2])
 	else:
 		device.send_cmd(args[2], args[3])
+
+def test_ping(args):
+
+	# Initialize and connect.
+	device = init(args[0], args[1])
+
+	# Ping.
+	latency = device.ping(args[2])
+
+	print(f"Latency: {latency}s")
 
 def main(args):
 
@@ -52,6 +69,7 @@ def main(args):
 		print("'o' means optional.")
 		print("__init__    ./test_common __init__ <oIP> <oUSER>")
 		print("send_cmd    ./test_common send_cmd <IP> <USER> <CMD> <oEXPECT>")
+		print("ping        ./test_ping ping <IP> <USER> <TARGET_IP>")
 
 		return -1
 
@@ -60,8 +78,12 @@ def main(args):
 		test_constructor(args[1::])
 
 	# Test send_cmd.
-	if args[0] == "send_cmd":
+	elif args[0] == "send_cmd":
 		test_send_cmd(args[1::])
+
+	# Test ping.
+	elif args[0] == "ping":
+		test_ping(args[1::])
 
 if __name__ == "__main__":
 	main(sys.argv[1::])
