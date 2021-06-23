d18872e11ba698a9006073e431d13a1541aaed97
Shawn Duong <shawnduong@protonmail.com>
2021-06-22 20:20:38 -0700

    Created manual unit tests for Brocade ICX 7450 command interface.

--- _test/manual/test_Brocade_ICX_7450.py
+++ _test/manual/test_Brocade_ICX_7450.py
@@ -28,6 +28,21 @@ def test_constructor(args):
 	print(f"device.ip         : {device.ip}")
 	print(f"device.connected  : {device.connected}")
 
+def test_send_cmd(args):
+
+	conf = config.config()
+
+	# Initialize and connect.
+	password  = getpass.getpass("(Test) Password: ")
+	secret    = getpass.getpass("(Test) Secret: ")
+	device    = Brocade_ICX_7450("ic", args[0], conf, args[1], password, secret)
+
+	# Send command.
+	if len(args) == 3:
+		device.send_cmd(args[2])
+	else:
+		device.send_cmd(args[2], args[3])
+
 def main(args):
 
 	# Help menu.
@@ -36,12 +51,17 @@ def main(args):
 		print("Manual Brocade ICX 7450 Tests")
 		print("'o' means optional.")
 		print("__init__    ./test_common __init__ <oIP> <oUSER>")
+		print("send_cmd    ./test_common send_cmd <IP> <USER> <CMD> <oEXPECT>")
 
 		return -1
 
 	# Test __init__.
 	if args[0] == "__init__":
 		test_constructor(args[1::])
+
+	# Test send_cmd.
+	if args[0] == "send_cmd":
+		test_send_cmd(args[1::])
 
 if __name__ == "__main__":
 	main(sys.argv[1::])
